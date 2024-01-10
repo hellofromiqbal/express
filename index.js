e28385798dbff6b695025046196c43dc890e00a0
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-01-10 14:02:09 +0700

    add new routes

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.json({ message: 'This json message is generated from "/" API route.' });
+});
+
+app.get("/hello", (req, res) => {
+  res.json({ message: 'This json message is generated from "/hello" API route.' });
+});
+
+app.get("/world", (req, res) => {
+  res.json({ message: 'This json message is generated from "/world" API route.' });
+});
+
+app.get("/:slug", (req, res) => {
+  const { slug } = this.request.params;
+  res.json({ message: `This json message is generated from "/${slug}" API route.` });
 });
 
 app.listen(5000, () => {
